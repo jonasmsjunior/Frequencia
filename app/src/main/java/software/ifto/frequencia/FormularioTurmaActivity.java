c76fa6d785551390cfa9730c330e1a58bf041634
jonasmsjunior <jonas.uft@gmail.com>
2016-11-16 23:34:13 -0200

    retorno a pagina inicial e mesndagem na tela

--- app/src/main/java/software/ifto/frequencia/FormularioTurmaActivity.java
+++ app/src/main/java/software/ifto/frequencia/FormularioTurmaActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class FormularioTurmaActivity extends AppCompatActivity {
@@ -19,5 +21,18 @@ public class FormularioTurmaActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_fomulario_turma, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioTurmaActivity.this, "Turma adicionada!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
