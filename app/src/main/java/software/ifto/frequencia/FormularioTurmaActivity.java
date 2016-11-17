7e1c14efb997a45497e38543e117e0b574bec54d
jonasmsjunior <jonas.uft@gmail.com>
2016-11-16 23:22:08 -0200

    modificações menu

--- app/src/main/java/software/ifto/frequencia/FormularioTurmaActivity.java
+++ app/src/main/java/software/ifto/frequencia/FormularioTurmaActivity.java
@@ -2,7 +2,8 @@ package software.ifto.frequencia;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuInflater;
 
 
 public class FormularioTurmaActivity extends AppCompatActivity {
@@ -11,5 +12,12 @@ public class FormularioTurmaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_turma);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_fomulario_turma, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
